bceba6f3592f19d27c589864fce9118b227df765
SourabhKumar2 <sourabhkumar2405@gmail.com>
2022-01-08 19:06:07 +0530

    Write test cases

--- handler.go
+++ handler.go
@@ -41,16 +41,28 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // a mapping of paths to urls.
 func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
 	var pathUrls []pathUrl
-	err := yaml.Unmarshal(yamlBytes, &pathUrls)
+	pathUrls, err := parseYaml(yamlBytes)
 	if err != nil {
 		return nil, err
 	}
-	// convert yam to map
+	return MapHandler(buildMap(pathUrls), fallback), nil
+}
+
+func buildMap(pathUrls []pathUrl) map[string]string {
 	pathsToUrls := make(map[string]string)
 	for _, pu := range pathUrls {
 		pathsToUrls[pu.Path] = pu.URL
 	}
-	return MapHandler(pathsToUrls, fallback), nil
+	return pathsToUrls
+}
+
+func parseYaml(data []byte) ([]pathUrl, error) {
+	var pathUrls []pathUrl
+	err := yaml.Unmarshal(data, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	return pathUrls, nil
 }
 
 type pathUrl struct {
